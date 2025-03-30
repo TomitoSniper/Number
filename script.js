979d3724974f551b67f1d94f46e409a023bcf3e9
Tomáš Hájek <tomas.hajek2007@seznam.cz>
2025-03-30 21:42:50 +0200

    30.3.

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
-// Links
 const numInput = document.getElementById("numInput");
 const output = document.getElementById("output");
 
-
-// Functions
-function countDigits(num) {
-    let digitCount = num.replace('-', '').length;
-
-    output.innerText += "Počet číslic: " + digitCount + "\n";
+function cifernySoucet(cislo) {
+    let soucet = 0;
+    let cisloString = String(cislo);
+    let ciselnyZnak;
+    for (let i = 0; i < cisloString.length; i++) {
+        ciselnyZnak = cisloString[i];
+        soucet += Number(ciselnyZnak);
+    }
+    return soucet;
 }
 
-function oddEven(num) {
-    if (num !== "") {
-        output.innerText += "Číslo je: " + ((num % 2 === 0) ? "Sudé" : "Liché") + "\n";
-    }
-}
-
-function isPrime(num) {
-    if (num < 2) {
-        output.innerText += "Je prvočíslo: Ne\n";  // If number is less than 2, it's not prime
-        return false;
-    }
-    
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            output.innerText += "Je prvočíslo: Ne\n";  // If divisible, it's not prime
+function palindrom(cislo) {
+    let cisloString = cislo.toString();
+    for (let i = 0; i < cisloString.length / 2; i++) {
+        if (cisloString[i] !== cisloString[cisloString.length - 1 - i]) {
             return false;
         }
     }
-    
-    output.innerText += "Je prvočíslo: Ano\n";  // If no divisors, it's prime
     return true;
 }
 
-function isPalindrome(num) {
-    let str = num.toString();
-    
-    let isPalindrom = str === str.split('').reverse().join('');
-    
-    output.innerText += "Je palindrom: " + (isPalindrom ? "Ano" : "Ne") + "\n";
-}
-
-function digitSum(num) {
-    if (num !== "") {
-        let sum = num.split('').reduce((acc, digit) => acc + parseInt(digit), 0);
-        
-        output.innerText += "Ciferný součet čísla: " + sum;
+function jeToPrvocislo(vstup) {
+    let cislo = parseInt(vstup);
+    if (cislo < 2) {
+        return false;
     }
+    if (cislo % 2 === 0 && cislo !== 2) {
+        return false;
+    }
+    let delitel = 3;
+    while (delitel <= Math.sqrt(cislo)) {
+        if (cislo % delitel === 0) {
+            return false;
+        }
+        delitel += 2;
+    }
+    return true;
 }
 
+function pocetCislic(cislo) {
+    return cislo.toString().length;
+}
 
-// Output
+function jeSude(cislo) {
+    return cislo % 2 === 0;
+}
 numInput.addEventListener("input", () => {
-    let num = numInput.value;
-    output.innerHTML = "";
+    const vstup = parseInt(numInput.value);
 
-    countDigits(num);
+    const pocet = pocetCislic(vstup);
+    const sudeLiche = jeSude(vstup) ? "sudé" : "liché";
+    const prvocislo = jeToPrvocislo(vstup) ? "ano" : "ne";
+    const jePalindrom = palindrom(vstup) ? "ano" : "ne";
+    const cifSoucet = cifernySoucet(vstup);
 
-    if (num !== "") {
-        oddEven(num);
-        isPrime(num);  // Only call for prime check if the input is a number (not empty string)
-        isPalindrome(num);
-        digitSum(num);
-    }
+    output.innerHTML = `
+        <p>Počet číslic: ${pocet}</p>
+        <p>Číslo je: ${sudeLiche}</p>
+        <p>Je prvočíslo: ${prvocislo}</p>
+        <p>Je palindrom: ${jePalindrom}</p>
+        <p>Ciferný součet: ${cifSoucet}</p>
+    `;
 });
